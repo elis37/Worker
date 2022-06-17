8bfbcca5ada306a1dd794cf6f3f48586cc133aa3
elis37 <104443314+elis37@users.noreply.github.com>
2022-06-17 12:19:24 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company.Netology;
+
+public class Main {
+
+  public static void main(String[] args) {
+	OnTaskDoneListener listener1 = System.out::println;
+	OnTaskErrorListener listener2 = System.out::println;
+	Worker worker = new Worker(listener1, listener2);
+	worker.start();
+    }
+}
